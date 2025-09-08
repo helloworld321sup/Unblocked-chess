872fa159d3c5f39d42e8ac7002d10862b33416ba
Blicket <jackson44554455@gmail.com>
2025-09-08 19:34:41 -0400

    Update mock-firebase.js

--- mock-firebase.js
+++ mock-firebase.js
@@ -1,14 +1,99 @@
 // Mock Firebase implementation for testing without real Firebase credentials
 // This allows the chess game to work locally for development and testing
 
-class MockFirebaseDatabase {
+// Mock Firebase Database implementation
+class MockFirebaseDatabaseImpl {
   constructor() {
     this.data = {};
     this.listeners = {};
   }
 
-  ref(path) {
-    return new MockFirebaseRef(this, path);
+  setValue(path, value) {
+    const keys = path.split('/');
+    let current = this.data;
+    
+    for (let i = 0; i < keys.length - 1; i++) {
+      if (!current[keys[i]]) {
+        current[keys[i]] = {};
+      }
+      current = current[keys[i]];
+    }
+    
+    current[keys[keys.length - 1]] = value;
+    
+    // Notify listeners
+    this.notifyListeners(path, value);
+  }
+
+  updateValue(path, updates) {
+    const keys = path.split('/');
+    let current = this.data;
+    
+    for (let i = 0; i < keys.length - 1; i++) {
+      if (!current[keys[i]]) {
+        current[keys[i]] = {};
+      }
+      current = current[keys[i]];
+    }
+    
+    const target = current[keys[keys.length - 1]] || {};
+    Object.assign(target, updates);
+    current[keys[keys.length - 1]] = target;
+    
+    // Notify listeners
+    this.notifyListeners(path, target);
+  }
+
+  getValue(path) {
+    const keys = path.split('/');
+    let current = this.data;
+    
+    for (const key of keys) {
+      if (current && typeof current === 'object' && key in current) {
+        current = current[key];
+      } else {
+        return null;
+      }
+    }
+    
+    return current;
+  }
+
+  addListener(path, listenerId, callback) {
+    if (!this.listeners[path]) {
+      this.listeners[path] = {};
+    }
+    this.listeners[path][listenerId] = callback;
+  }
+
+  removeListener(path, listenerId) {
+    if (this.listeners[path]) {
+      delete this.listeners[path][listenerId];
+      if (Object.keys(this.listeners[path]).length === 0) {
+        delete this.listeners[path];
+      }
+    }
+  }
+
+  notifyListeners(path, value) {
+    // Notify exact path listeners
+    if (this.listeners[path]) {
+      Object.values(this.listeners[path]).forEach(callback => {
+        setTimeout(() => callback({ val: () => value }), 0);
+      });
+    }
+    
+    // Notify parent path listeners
+    const pathParts = path.split('/');
+    for (let i = pathParts.length - 1; i > 0; i--) {
+      const parentPath = pathParts.slice(0, i).join('/');
+      if (this.listeners[parentPath]) {
+        const parentValue = this.getValue(parentPath);
+        Object.values(this.listeners[parentPath]).forEach(callback => {
+          setTimeout(() => callback({ val: () => parentValue }), 0);
+        });
+      }
+    }
   }
 }
 
@@ -112,167 +197,31 @@ class MockFirebaseQueryWithFilter {
   }
 }
 
-// Mock Firebase Database implementation
-class MockFirebaseDatabaseImpl {
+class MockFirebaseDatabase {
   constructor() {
-    this.data = {};
-    this.listeners = {};
+    this.mockDb = new MockFirebaseDatabaseImpl();
   }
 
-  setValue(path, value) {
-    const keys = path.split('/');
-    let current = this.data;
-    
-    for (let i = 0; i < keys.length - 1; i++) {
-      if (!current[keys[i]]) {
-        current[keys[i]] = {};
-      }
-      current = current[keys[i]];
-    }
-    
-    current[keys[keys.length - 1]] = value;
-    
-    // Notify listeners
-    this.notifyListeners(path, value);
-  }
-
-  updateValue(path, updates) {
-    const keys = path.split('/');
-    let current = this.data;
-    
-    for (let i = 0; i < keys.length - 1; i++) {
-      if (!current[keys[i]]) {
-        current[keys[i]] = {};
-      }
-      current = current[keys[i]];
-    }
-    
-    const target = current[keys[keys.length - 1]] || {};
-    Object.assign(target, updates);
-    current[keys[keys.length - 1]] = target;
-    
-    // Notify listeners
-    this.notifyListeners(path, target);
-  }
-
-  getValue(path) {
-    const keys = path.split('/');
-    let current = this.data;
-    
-    for (const key of keys) {
-      if (current && typeof current === 'object' && key in current) {
-        current = current[key];
-      } else {
-        return null;
-      }
-    }
-    
-    return current;
-  }
-
-  addListener(path, listenerId, callback) {
-    if (!this.listeners[path]) {
-      this.listeners[path] = {};
-    }
-    this.listeners[path][listenerId] = callback;
-  }
-
-  removeListener(path, listenerId) {
-    if (this.listeners[path]) {
-      delete this.listeners[path][listenerId];
-      if (Object.keys(this.listeners[path]).length === 0) {
-        delete this.listeners[path];
-      }
-    }
-  }
-
-  notifyListeners(path, value) {
-    // Notify exact path listeners
-    if (this.listeners[path]) {
-      Object.values(this.listeners[path]).forEach(callback => {
-        setTimeout(() => callback({ val: () => value }), 0);
-      });
-    }
-    
-    // Notify parent path listeners
-    const pathParts = path.split('/');
-    for (let i = pathParts.length - 1; i > 0; i--) {
-      const parentPath = pathParts.slice(0, i).join('/');
-      if (this.listeners[parentPath]) {
-        const parentValue = this.getValue(parentPath);
-        Object.values(this.listeners[parentPath]).forEach(callback => {
-          setTimeout(() => callback({ val: () => parentValue }), 0);
-        });
-      }
-    }
+  ref(path) {
+    return new MockFirebaseRef(this.mockDb, path);
   }
 }
 
-// Initialize mock Firebase if real Firebase is not available
-if (typeof firebase === 'undefined' || !firebase.apps || firebase.apps.length === 0) {
-  console.log('Using mock Firebase for local development');
-  
-  // Create mock firebase object
-  window.firebase = {
-    initializeApp: function(config) {
-      console.log('Mock Firebase initialized with config:', config);
-      return {
-        database: function() {
-          return new MockFirebaseDatabase();
-        }
-      };
-    },
-    apps: []
-  };
-  
-  // Create mock database
-  const mockDb = new MockFirebaseDatabaseImpl();
-  window.firebaseDatabase = new MockFirebaseDatabase();
-  
-  // Override the database methods to use our mock implementation
-  const originalRef = window.firebaseDatabase.ref;
-  window.firebaseDatabase.ref = function(path) {
-    const ref = originalRef.call(this, path);
-    
-    // Override methods to use mock implementation
-    ref.set = function(value) {
-      return new Promise((resolve) => {
-        mockDb.setValue(path, value);
-        resolve();
-      });
+// Initialize mock Firebase
+console.log('Using mock Firebase for local development');
+
+// Create mock firebase object
+window.firebase = {
+  initializeApp: function(config) {
+    console.log('Mock Firebase initialized with config:', config);
+    return {
+      database: function() {
+        return new MockFirebaseDatabase();
+      }
     };
-    
-    ref.update = function(updates) {
-      return new Promise((resolve) => {
-        mockDb.updateValue(path, updates);
-        resolve();
-      });
-    };
-    
-    ref.once = function(eventType) {
-      return new Promise((resolve) => {
-        const value = mockDb.getValue(path);
-        resolve({ val: () => value });
-      });
-    };
-    
-    ref.on = function(eventType, callback) {
-      const listenerId = Math.random().toString(36);
-      mockDb.addListener(path, listenerId, callback);
-      
-      // Return unsubscribe function
-      return () => {
-        mockDb.removeListener(path, listenerId);
-      };
-    };
-    
-    ref.off = function(eventType) {
-      // Remove all listeners for this path
-      Object.keys(mockDb.listeners[path] || {}).forEach(listenerId => {
-        mockDb.removeListener(path, listenerId);
-      });
-    };
-    
-    return ref;
-  };
-}
+  },
+  apps: []
+};
+
+// Create mock database
+window.firebaseDatabase = new MockFirebaseDatabase();
